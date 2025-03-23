3ea5419f60660b2d21c9d1f1c33abf9069a2ee44
Dharmateja69 <ddharmateja69@gmail.com>
2025-03-23 15:27:00 +0530

    Demo getsture

--- former-motion/src/App.tsx
+++ former-motion/src/App.tsx
@@ -1,9 +1,25 @@
-import Filipingcard from "./Components/Filipingcard";
-
+import { motion } from "framer-motion";
 const App = () => {
   return (
     <div>
-      <Filipingcard />
+      {/* while Hover */}
+      {/* <motion.div
+        className="box"
+        whileHover={{ scale: 1.2, rotate: 10 }}
+        transition={{ type: "spring", stiffness: 300 }}
+      /> */}
+      {/* whileTap */}
+      {/* <motion.div
+        className="box"
+        whileTap={{ scale: 0.8, backgroundColor: "crimson" }}
+        transition={{ type: "spring", stiffness: 300 }}
+      /> */}
+      {/* whiledrag */}
+      <motion.div
+        className="box"
+        drag
+        dragConstraints={{ top: -50, left: -50, right: 50, bottom: 50 }}
+      />
     </div>
   );
 };
