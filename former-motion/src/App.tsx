e5ebc6fedb39e3300734ed78a558e6d470e9fd4a
Dharmateja69 <ddharmateja69@gmail.com>
2025-03-23 15:16:24 +0530

    Variants ex2

--- former-motion/src/App.tsx
+++ former-motion/src/App.tsx
@@ -1,20 +1,9 @@
-import { motion } from "framer-motion";
-import { useState } from "react";
-import { variants } from "./Components/variants";
+import Filipingcard from "./Components/Filipingcard";
 
 const App = () => {
-  const [isvsiable, setisvisiable] = useState(true);
   return (
     <div>
-      <motion.div
-        className="box"
-        variants={variants}
-        initial="hidden"
-        animate={isvsiable ? "visible" : "hidden"}
-        exit="exit"
-        transition={{ duration: 1 }}
-        onClick={() => setisvisiable(!isvsiable)}
-      ></motion.div>
+      <Filipingcard />
     </div>
   );
 };
