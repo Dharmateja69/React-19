d8f428040fdb240d07faf42f727c83f7da18af45
Dharmateja69 <ddharmateja69@gmail.com>
2025-03-28 18:27:08 +0530

    Zustand

--- former-motion/src/App.tsx
+++ former-motion/src/App.tsx
@@ -1,40 +1,13 @@
-import ProgressSteps from "./Projects/ProgressSteps/ProgresSteps";
+import OtherComponent from "./ZustandStore/OtherComponent";
+import { useCounter } from "./ZustandStore/Store";
 
 const App = () => {
+  const count = useCounter((state) => state.count);
+
   return (
     <div>
-      {/* <ScrollIndicator /> */}
-      {/* <Loader /> */}
-      {/* <div className="flex justify-center items-center h-[100vh] bg-[#f3f3f3]">
-        <DraggableCards
-          style={{
-            background: "linear-gradient(135deg, #f093fb 0%, #f5576c 100%)",
-          }}
-        >
-          Card 1
-        </DraggableCards>
-        <DraggableCards
-          style={{
-            background: "linear-gradient(135deg, #5ee7df 0%, #b490ca 100%)",
-          }}
-        >
-          Card 2
-        </DraggableCards>
-        <DraggableCards
-          style={{
-            background: "linear-gradient(135deg, #ff9a9e 0%, #fecfef 99%)",
-          }}
-        >
-          Card 3
-        </DraggableCards>
-      </div> */}
-
-      {/* <Carousl /> */}
-      {/* <Navbar /> */}
-      {/* <ScrollAnimation /> */}
-      {/* <ScrollCarousel /> */}
-      {/* <StickyNav /> */}
-      <ProgressSteps />
+      <h1>Count: {count}</h1>
+      <OtherComponent />
     </div>
   );
 };
