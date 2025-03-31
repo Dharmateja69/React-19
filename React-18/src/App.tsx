8918bf7861e3802e7deac0294702626578446323
Dharmateja69 <ddharmateja69@gmail.com>
2025-04-01 00:31:36 +0530

    WatchStore

--- React-18/src/App.tsx
+++ React-18/src/App.tsx
@@ -1,102 +1,45 @@
-import { MdMoreVert } from "react-icons/md";
-import MainArea from "./Projects/Advancedlist/MainArea";
-import { Modal } from "./Projects/Advancedlist/Modal";
-import Sidebar from "./Projects/Advancedlist/Sidebar";
-import { UseAppStore } from "./Projects/Advancedlist/Store";
+import ProductCard from "./Projects/Watchstore/Components/ProductCard";
+import Sidebar from "./Projects/Watchstore/Components/Sidebar";
+import { data } from "./Projects/Watchstore/Db/Db";
+import { useFilterStore } from "./Projects/Watchstore/Store";
+interface Product {
+  id: string;
+  country: string;
+  img: Record<string, string>;
+  price: number;
+}
 
-function App() {
-  const {
-    todos,
-    editIndex,
-    editText,
-    dropdownIndex,
-    handleEdit,
-    handleUpdate,
-    handleDropdownClick,
-    deleteTodo,
-    setEditText,
-    setEditIndex,
-  } = UseAppStore();
+const App = () => {
+  const { selectedCountries, selectedColors, selectedPriceRange } =
+    useFilterStore((state) => state);
+
+  const filteredData = data.filter((item: Product) => {
+    const matchesColor =
+      selectedColors.length === 0 ||
+      Object.keys(item.img).some((color) => selectedColors.includes(color));
+
+    const matchesCountry =
+      selectedCountries.length === 0 ||
+      selectedCountries.includes(item.country);
+
+    const matchesPrice = selectedPriceRange
+      ? item.price >= selectedPriceRange.min &&
+        item.price <= selectedPriceRange.max
+      : true;
+
+    return matchesColor && matchesCountry && matchesPrice;
+  });
 
   return (
-    <div className="flex h-screen">
+    <>
       <Sidebar />
-      <div className="flex-1 p-6">
-        <MainArea />
-        <div className="mt-6">
-          <h2 className="text-xl font-semibold mb-4 ml-[2rem]">Todo List</h2>
-          <ul className="list-disc pl-5">
-            {todos.map((todo, index) => (
-              <li key={index} className="mb-2 ml-[2rem]">
-                {editIndex === index ? (
-                  <div className="flex items-center">
-                    <input
-                      type="text"
-                      value={editText}
-                      onChange={(e) => setEditText(e.target.value)}
-                      className="border border-gray-300 p-1 rounded-lg mr-2"
-                    />
-                    <button
-                      onClick={() => handleUpdate(index)}
-                      className="bg-green-500 text-black px-2 py-1
-                       rounded-lg mr-2"
-                    >
-                      Update
-                    </button>
-                    <button
-                      onClick={() => setEditIndex(null)}
-                      className="bg-gray-500 text-black px-2 py-1
-                       rounded-lg"
-                    >
-                      Cancel
-                    </button>
-                  </div>
-                ) : (
-                  <div className="relative flex justify-between items-center">
-                    <div>
-                      <span className="mr-4">
-                        <strong>{todo.name}</strong> (List: {todo.list},
-                        Workspace: {todo.workspace})
-                      </span>
-                    </div>
-                    <div className="flex items-center">
-                      <MdMoreVert
-                        onClick={() => handleDropdownClick(index)}
-                        size={24}
-                        className="cursor-pointer"
-                      />
-                      {dropdownIndex === index && (
-                        <div
-                          className="absolute right-0 mt-2 bg-white
-                         border rounded shadow-lg"
-                        >
-                          <button
-                            onClick={() => handleEdit(index)}
-                            className="block px-4 py-2 text-gray-700
-                             hover:bg-gray-100 w-full text-left"
-                          >
-                            Update
-                          </button>
-                          <button
-                            onClick={() => deleteTodo(index)}
-                            className="block px-4 py-2 text-gray-700
-                             hover:bg-gray-100 w-full text-left"
-                          >
-                            Delete
-                          </button>
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                )}
-              </li>
-            ))}
-          </ul>
-        </div>
+      <div className="p-4 flex flex-wrap justify-center items-center">
+        {filteredData.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
       </div>
-      <Modal />
-    </div>
+    </>
   );
-}
+};
 
 export default App;
