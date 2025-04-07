2cf9d59210099fb595e67f764259cae650ee9bdf
Dharmateja69 <ddharmateja69@gmail.com>
2025-04-07 21:19:41 +0530

    modified

--- React-18/src/App.tsx
+++ React-18/src/App.tsx
@@ -1,27 +1,31 @@
-import Gameinfo from "./ReactDesign_PATTERNS/Listofdata/Components/Gameinfo";
-import MOviesInfo from "./ReactDesign_PATTERNS/Listofdata/Components/MOviesInfo";
-import Rednderlist from "./ReactDesign_PATTERNS/Listofdata/Components/Rednderlist";
-import { games, movies } from "./ReactDesign_PATTERNS/Listofdata/data";
-const s = () => {
+import {
+  clothing,
+  electronics,
+  homeGoods,
+} from "./ReactDesign_PATTERNS/Challenges/Listdata/Data";
+import ProductInfo from "./ReactDesign_PATTERNS/Challenges/Listdata/ProductInfo";
+import Renderlist from "./ReactDesign_PATTERNS/Challenges/Listdata/Renderlist";
+
+const App = () => {
   return (
     <>
-      <Rednderlist data={games} resourceName="games" dataToRender={Gameinfo} />
-      {/* data={games}
-      You're passing an array or list named games. This is the actual data you
-      want to display. resourceName="games" This is just a label or a name to
-      identify what kind of data you're dealing with. It can be used inside the
-      component to make the code more flexible. dataToRender={Gameinfo}
-      You're passing a component (Gameinfo) that will render each item in the
-      list. So Rednderlist will loop over the data and render Gameinfo for each
-      game */}
-      <hr></hr>
-      <Rednderlist
-        data={movies}
-        resourceName="movies"
-        dataToRender={MOviesInfo}
+      <Renderlist
+        data={electronics}
+        resourceName="product"
+        DataToRender={ProductInfo}
+      />
+      <Renderlist
+        data={clothing}
+        resourceName="product"
+        DataToRender={ProductInfo}
+      />
+      <Renderlist
+        data={homeGoods}
+        resourceName="product"
+        DataToRender={ProductInfo}
       />
     </>
   );
 };
 
-export default s;
+export default App;
