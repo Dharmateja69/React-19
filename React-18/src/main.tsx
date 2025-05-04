157a402c6b06da5f41de5e9cc8b47507138a7ad0
Dharmateja69 <ddharmateja69@gmail.com>
2025-05-05 03:21:39 +0530

    Modified

--- React-18/src/main.tsx
+++ React-18/src/main.tsx
@@ -1,14 +1,9 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
-import SidebarProvider from "./Projects/SimplePortfolio/Provider/SidebarProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   // ✅ CORRECT (main.tsx or App.tsx)
-  <BrowserRouter>
-    <SidebarProvider>
-      <App />
-    </SidebarProvider>
-  </BrowserRouter>
+
+  <App />
 );
